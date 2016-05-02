e96d736adfc9fa79ff99873884106b729ea95f67
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-05-02 14:03:23 -0400

    Add module

--- zoo.rb
+++ zoo.rb
@@ -1,4 +1,18 @@
+
+module Action
+
+  def fly
+    puts "I'm a #{self.class}, watch me fly!" if self.can_fly?
+  end
+
+  def swim
+    puts "I'm a #{self.class}, watch me swim!" if self.can_swim?
+  end
+
+end
+
 class Animal
+  include Action
   attr_accessor :num_legs, :warm_blood, :can_swim, :can_fly
 
   def initialize(num_legs,warm_blood,can_swim,can_fly)
@@ -42,8 +56,7 @@ end
 
 class Chimpanzee < Primate
   def initialize
-    can_swim = false
-    super(can_swim)
+    super(can_swim = false)
   end
 end
 
@@ -53,7 +66,8 @@ puts "Is a chimpanzee warm blooded? #{koko.warm_blooded? ?  'Yes' : 'No'}"
 puts "How many legs does a himpanzee have? #{koko.num_legs}"
 puts "Can a chimpanzee fly? #{koko.can_fly? ?  'Yes' : 'No'}"
 puts "Can a chimpanzee swim? #{koko.can_swim? ?  'Yes' : 'No'}"
-
+koko.swim
+koko.fly
 
 # MYTHICAL SIMMER CHIMPANZEES
 
@@ -70,7 +84,8 @@ puts "Is a swimmer chimpanzee warm blooded? #{waterkoko.warm_blooded? ? 'Yes' : 
 puts "How many legs does a swimmer chimpanzee have? #{waterkoko.num_legs}"
 puts "Can a swimmer chimpanzee fly? #{waterkoko.can_fly? ?  'Yes' : 'No'}"
 puts "Can a swimmer chimpanzee swim? #{waterkoko.can_swim? ?  'Yes' : 'No'}"
-
+waterkoko.swim
+waterkoko.fly
 
 # BIRDS
 
@@ -99,6 +114,8 @@ puts "Is a parrot warm blooded? #{sam.warm_blooded? ? 'Yes' : 'No'}"
 puts "How many legs does a parrto have? #{sam.num_legs}"
 puts "Can a parrto fly? #{sam.can_fly? ?  'Yes' : 'No'}"
 puts "Can a parrto swim? #{sam.can_swim? ?  'Yes' : 'No'}"
+sam.fly
+sam.swim
 
 
 # DUCKS
@@ -116,6 +133,8 @@ puts "Is a duck warm blooded? #{ducky.warm_blooded? ? 'Yes' : 'No'}"
 puts "How many legs does a bat have? #{ducky.num_legs}"
 puts "Can a duck fly? #{ducky.can_fly? ?  'Yes' : 'No'}"
 puts "Can a duck swim? #{ducky.can_swim? ?  'Yes' : 'No'}"
+ducky.fly
+ducky.swim
 
 
 # AMPHIBIANS
@@ -142,6 +161,8 @@ puts "Is a sam warm blooded? #{frogy.warm_blooded? ? 'Yes' : 'No'}"
 puts "How many legs does a bat have? #{frogy.num_legs}"
 puts "Can a frog fly? #{frogy.can_fly? ?  'Yes' : 'No'}"
 puts "Can a frog swim? #{frogy.can_swim? ?  'Yes' : 'No'}"
+frogy.fly
+frogy.swim
 
 
 # BATS
@@ -161,3 +182,5 @@ puts "Is a bat warm blooded? #{batty.warm_blooded? ? 'Yes' : 'No'}"
 puts "How many legs does a bat have? #{batty.num_legs}"
 puts "Can a bat fly? #{batty.can_fly? ?  'Yes' : 'No'}"
 puts "Can a bat swim? #{batty.can_swim? ?  'Yes' : 'No'}"
+batty.fly
+batty.swim
